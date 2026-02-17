e065d85443e92e2deefa3e0480132375795f097a
Talal Mahmud <mahmudtalal2@gmail.com>
2026-02-17 14:57:30 +0600

    updated

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const mailTransporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: true, // true for 465, false for 587
+  secure: false, // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
