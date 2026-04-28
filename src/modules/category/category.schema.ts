2dad7514cd0aa7bd5041383fa5a324c3a5980050
Talalmahmud <mahmudtalal2@gmail.com>
2026-04-28 12:29:41 +0600

    feat: add isActive field to create and update category schemas with boolean preprocessing

--- src/modules/category/category.schema.ts
+++ src/modules/category/category.schema.ts
@@ -1,14 +1,21 @@
 import { z } from "zod";
+import { is } from "zod/v4/locales";
 
 export const createCategorySchema = z.object({
   body: z.object({
     name: z.string().min(2),
+    isActive: z
+      .preprocess((val) => val === "true" || val === true, z.boolean())
+      .optional(),
   }),
 });
 
 export const updateCategorySchema = z.object({
   body: z.object({
     name: z.string().min(2).optional(),
+    isActive: z
+      .preprocess((val) => val === "true" || val === true, z.boolean())
+      .optional(),
   }),
   params: z.object({
     id: z.string(),
